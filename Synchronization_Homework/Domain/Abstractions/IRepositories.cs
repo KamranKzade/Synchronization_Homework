2efb0aa33da5580fa5efdb0352aadce098f60104
Kamran <311.kamran@gmail.com>
2023-03-10 02:59:46 +0400

    Add project files.

---
+++ Synchronization_Homework/Domain/Abstractions/IRepositories.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+
+namespace Synchronization_Homework.Domain.Abstractions
+{
+    public interface IRepository<T>
+    {
+        T GetData(int id);
+        ObservableCollection<T> GetAllData();
+        void AddData(T data);
+        void UpdateData(T data);
+        void DeleteData(T data);
+    }
+}
